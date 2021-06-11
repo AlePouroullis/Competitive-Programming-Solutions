b4aa6180d3381350fe45965afbbd2dcbb8ae7ff1
Alexandros Pouroullis <69683229+AlePouroullis@users.noreply.github.com>
2021-06-11 16:54:19 +0200

    Update Superprime Rib (1994).cpp

--- USACO/Competitions/Superprime Rib (1994).cpp
+++ USACO/Competitions/Superprime Rib (1994).cpp
@@ -20,7 +20,7 @@ bool is_prime(int num){
     if(num < 2) return false;
     if(num == 2 || num == 3) return true;
     if(!(num % 6 == 1 || num % 6 == 5)) return false;
-    for(int i = 2; i*i < num; i++)
+    for(int i = 2; i*i <= num; i++)
         if(num % i == 0)
             return false;
     return true;

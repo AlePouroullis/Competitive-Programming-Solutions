70dde6b2952f776c6a729343741e9df52553b20b
Alexandros Pouroullis <69683229+AlePouroullis@users.noreply.github.com>
2021-06-24 22:38:20 +0200

    Update Dice Combinations.cpp

--- CSES/Dice Combinations.cpp
+++ CSES/Dice Combinations.cpp
@@ -5,24 +5,12 @@ using namespace std;
 int main(){
     int n;
     cin >> n;
-    char grid[n][n];
-    for(int i = 0; i < n; i++)
-    for(int j = 0; j < n; j++)
-        cin >> grid[i][j];
-
-    ll dp[n][n];
-    memset(dp, 0, sizeof(dp));
-    if(grid[0][0] != '*')
-        dp[0][0] = 1;
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            if(grid[i][j] != '*'){
-                if(i > 0)
-                    dp[i][j] += dp[i-1][j];
-                if(j > 0)
-                    dp[i][j] = (dp[i][j] + dp[i][j-1])%(1000000007);
-            }
+    ll dp[n + 1] = {0};
+    dp[0] = 1;
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= min(6, i); j++){
+            dp[i] = (dp[i] + dp[i-j])%(1000000007);
         }
     }
-    cout << dp[n-1][n-1] << endl;
+    cout << dp[n];
 }
